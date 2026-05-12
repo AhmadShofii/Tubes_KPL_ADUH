77f5b8e13631798741367eac1abfe274a3c6b4a2
Aditio Nugroho <125227036+aditionugroho@users.noreply.github.com>
2026-05-12 14:21:48 +0700

    Delete TUBES_KPL/Frontend directory

--- TUBES_KPL/Frontend/keranjang/index.jsx
+++
@@ -1,5 +0,0 @@
-// Entry point untuk fitur Keranjang Saya
-// Import komponen ini di App.jsx atau routing utama
-export { default as CartPage } from "./components/CartPage";
-export { default as CartItem } from "./components/CartItem";
-export { default as OrderSummary } from "./components/OrderSummary";
